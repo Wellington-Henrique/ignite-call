cdef3ba053664b7275ff859bfcba3c8645f86b0b
Wellington Henrique <wellingtonhlc@gmail.com>
2025-04-02 14:18:50 -0300

    refactor: tratamento de data

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -11,10 +11,12 @@ export default async function handler(
   }
 
   const username = String(req.query.username);
-  const { date } = req.query;
+  const { date, timezoneOffset } = req.query;
 
-  if (!date) {
-    return res.status(400).json({ message: "Date no provided." });
+  if (!date || timezoneOffset) {
+    return res
+      .status(400)
+      .json({ message: "Date or timezoneOffset not provided." });
   }
 
   const user = await prisma.user.findUnique({
@@ -28,6 +30,15 @@ export default async function handler(
   }
 
   const referenceDate = dayjs(String(date));
+
+  const timezoneOffsetInHours =
+    typeof timezoneOffset === "string"
+      ? Number(timezoneOffset) / 60
+      : Number(timezoneOffset[0]) / 60;
+
+  const referenceDateTimezoneOffsetInHours =
+    referenceDate.toDate().getTimezoneOffset() / 60;
+
   const isPastDate = referenceDate.endOf("day").isBefore(new Date());
 
   if (isPastDate) {
@@ -63,8 +74,14 @@ export default async function handler(
     where: {
       user_id: user.id,
       date: {
-        gte: referenceDate.set("hour", startHour).toDate(),
-        lte: referenceDate.set("hour", endHour).toDate(),
+        gte: referenceDate
+          .set("hour", startHour)
+          .add(timezoneOffsetInHours, "hours")
+          .toDate(),
+        lte: referenceDate
+          .set("hour", endHour)
+          .add(timezoneOffsetInHours, "hours")
+          .toDate(),
       },
     },
   });
